d286c9c2e7e741c7c3f9c72391ae51991a7707cd
Dustin Cook <dustin.cook@mines.sdsmt.edu>
2018-09-23 02:14:05 -0600

    init commit

---
+++ PRG1/circuit.c
@@ -0,0 +1,93 @@
+/* File:    circuit.c
+ * Author:  Dustin Cook
+ * Purpose: Try's every combination of inputs for the circuit.
+ *
+ * Input:   thread_count
+ * Output:  Successful input combinations, the thread that made the successful combination,
+ *          and the timeing.
+ *
+ * Compile: gcc -g -Wall -fopenmp -o circuit circuit.c
+ * Usage:   ./main <number of threads> <>
+ *
+ * Notes: There does appear to be a small performance boost when using static vs dynamic.
+ *
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+
+int check_circuit (int id, int z);
+void Usage(char* prog_name);
+
+int main(int argc, char *argv[])
+{
+  double averageTime = 0.0;
+  const unsigned int MAX_VALUE = 65535;
+  unsigned int i;
+  int thread_count;
+  int count = 0;
+  double start = omp_get_wtime();
+  if (argc != 2) Usage(argv[0]);
+  thread_count = strtol(argv[1], NULL, 10);
+
+#   pragma omp parallel for num_threads(thread_count) schedule( dynamic, 1 ) reduction(+:count, averageTime)
+    for (i = 0; i <= MAX_VALUE; i++)
+    {
+      double wtime = omp_get_wtime();
+       if (check_circuit(omp_get_thread_num(), i))
+         count += 1;
+       wtime = omp_get_wtime() - wtime;
+       averageTime += wtime;
+    }
+    // barrier already exists
+  # pragma omp single
+  {
+    averageTime /= MAX_VALUE;
+    printf("count: %d\naverage time per thread: %f\n", count, averageTime);
+    printf("program time: %f\n", omp_get_wtime() - start);
+  }
+
+  return 0;
+}
+
+
+/* Return 1 if 'i'th bit of 'n' is 1; 0 otherwise */
+ #define EXTRACT_BIT(n,i) ((n&(1<<i))?1:0)
+ /* Check if a given input produces TRUE (a one) */
+ int check_circuit (int id, int z)
+ {
+  int v[16]; /* Each element is a bit of z */
+  int i;
+   for (i = 0; i < 16; i++) v[i] = EXTRACT_BIT(z,i);
+   if ((v[0] || v[1]) && (!v[1] || !v[3]) && (v[2] || v[3])
+     && (!v[3] || !v[4]) && (v[4] || !v[5])
+     && (v[5] || !v[6]) && (v[5] || v[6])
+     && (v[6] || !v[15]) && (v[7] || !v[8])
+     && (!v[7] || !v[13]) && (v[8] || v[9])
+     && (v[8] || !v[9]) && (!v[9] || !v[10])
+     && (v[9] || v[11]) && (v[10] || v[11])
+     && (v[12] || v[13]) && (v[13] || !v[14])
+     && (v[14] || v[15])) {
+     /*printf ("%d) %d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n", id,
+     v[0],v[1],v[2],v[3],v[4],v[5],v[6],v[7],v[8],v[9],
+     v[10],v[11],v[12],v[13],v[14],v[15]);
+     fflush (stdout); */
+     return 1;
+   } else return 0;
+ }
+
+ /*--------------------------------------------------------------------
+ * Function:    Usage
+ * Purpose:     Print command line for function and terminate
+ * In arg:      prog_name
+ */
+void Usage(char* prog_name)
+{
+   fprintf(stderr, "usage: %s <number of threads>\n", prog_name);
+   exit(0);
+}  /* Usage */
---
+++ PRG1/soe.c
@@ -0,0 +1,114 @@
+/* File:    soe.c
+ * Author:  Dustin Cook
+ * Purpose: Finds all primes up to given n.
+ *
+ * Input:   n
+ * Output:  All primes leading up to n in O(n) time.
+ *
+ * Compile: gcc -g -Wall -fopenmp -o soe soe.c -lm
+ * Usage:   ./soe <n> <Print Input: 1 for yes, 0 for no>
+ *
+ * Notes: There does appear to be a performance boost of around .002 seconds when using
+ *        dynamic vs static.
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+#define false 0
+#define true 1
+
+
+void Usage(char* prog_name);
+void printDivider();
+void printPrimeTable(char * marks, int size, int n);
+
+
+int main(int argc, char *argv[])
+{
+  int n;
+  int p;
+  int longRuns = 0;
+  int printInput = 0;
+  double start = omp_get_wtime();
+  double averageTime = 0.0;
+  int thread_count = 8;
+  n = strtol(argv[1], NULL, 10);
+  int size = n + 1;
+  char marks[size];
+
+  marks[0] = marks[1] = false;
+  for (int i = 2; i < size; i++) {
+    marks[i] = true;
+  }
+
+  printf("n: %d, size: %d, thread_count: %d\n", n, size, thread_count);
+
+  # pragma omp parallel for num_threads(thread_count) schedule( static, 1 ) reduction(+:averageTime)
+  for (p = 2; p < size; p++) {
+    int local_p = false;
+    # pragma omp critical (validateP)
+    {
+      if (marks[p] == true)
+        local_p = p;
+    }
+    if (local_p != false) {
+      for (int i = local_p * 2; i <= size; i += local_p) {
+           marks[i] = false;
+      }
+    }
+
+    if (local_p != false) {
+      averageTime += omp_get_wtime() - start;
+      longRuns++;
+    }
+  }
+
+  # pragma omp single
+  {
+    if (printInput == true)
+      printPrimeTable(marks, size, n);
+    else {
+      averageTime /= longRuns;
+      printf("average thread time: %f\ntotal time: %f\n", averageTime, omp_get_wtime() - start);
+    }
+  }
+
+
+  return 0;
+}
+
+
+ /*--------------------------------------------------------------------
+ * Function:    Usage
+ * Purpose:     Print command line for function and terminate
+ * In arg:      prog_name
+ */
+void Usage(char* prog_name)
+{
+   fprintf(stderr, "usage: %s <n> <Print Input: 1 for yes, 0 for no>\n", prog_name);
+   exit(0);
+}  /* Usage */
+
+void printPrimeTable(char * marks, int size, int n) {
+  printf("Prime List | %d", n);
+  printDivider();
+  printf("%d: ", 0);
+  for (int i = 0; i < size; i++) {
+    if ((i + 1) % 50 == 0) {
+      printDivider();
+      printf("%d's Range: ", i+1);
+    }
+    if (marks[i] == true)
+      printf("%d | ", i);
+  }
+  printDivider();
+}
+
+void printDivider() {
+  printf("\n--------------------------------------------------------------------------------\n");
+}
