8915a9acb8ba5e7adbbaa1ca407b2e6440e4d660
Dustin Cook <dustin.cook@mines.sdsmt.edu>
2018-12-09 18:32:15 -0700

    serial is kind of faster than parallel...

--- final/test.py
+++ final/test.py
@@ -5,12 +5,26 @@ import random
 import datetime
 
 
-subprocess.run(["make"], stdout=subprocess.PIPE, universal_newlines=True)
+subprocess.run(["make", "parallel"], universal_newlines=True)
+subprocess.run(["make", "serial"], universal_newlines=True)
 
 
 for i in range(1, 17):
+	
+	# RUN PARALLEL #
 	start = datetime.datetime.now()		
-	output = subprocess.run(["mpirun", "-np", "15", "./parallel", str(i), "0"], universal_newlines=True).stdout
+	p_output = subprocess.run(["mpirun", "-np", "4", "./parallel", str(i), "0"], stdout=subprocess.PIPE, universal_newlines=True).stdout
 	end = datetime.datetime.now()
-	runtime = end - start
-	print(str(i) + "X" + str(i) + "\n" + "Processors: 15" + " took: " + str(runtime.total_seconds()) + " microseconds\n" + output)
+	parallel_runtime = end - start
+
+
+	# RUN SERIAL #
+	start = datetime.datetime.now()		
+	s_output = subprocess.run(["./serial", str(i)], stdout=subprocess.PIPE, universal_newlines=True).stdout
+	end = datetime.datetime.now()
+	serial_runtime = end - start
+
+	print(str(i) + "X" + str(i))
+	print("Parallel took: " + str(parallel_runtime.total_seconds()) + " microseconds")
+	print("Result: " + p_output)
+	print("Serial took: " + str(serial_runtime.total_seconds()) + " microseconds\nResult: " + s_output)
